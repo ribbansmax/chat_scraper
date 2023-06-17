bface76f134a9a2c6957f00e0ceafe0df4c6dff7
ribbansmax <ribbans.max@gmail.com>
2023-06-17 11:46:46 -0500

    move

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_caching import Cache
-app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-
-from app import routes
-
-if __name__ == '__main__':
-    app.run()
