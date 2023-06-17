1472ae7a8a4918239b27f27869fe2e00f2bf7336
ribbansmax <ribbans.max@gmail.com>
2023-06-17 11:18:36 -0500

    cleanup

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-from bs4 import BeautifulSoup
-import requests
-
-class WciaContent():
-    URL = 'https://www.wcia.com/news/local-news/'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
